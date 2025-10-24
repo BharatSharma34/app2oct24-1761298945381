a73deba2d50a96aea866636915de5a2b7f143db5
BharatSharma34 <bharatsharma@ducktaleit.com>
2025-10-24 15:18:42 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { api } from "../utils/api";
+
 export default function Home() {
   const [data, setData] = useState(null);
-  useEffect(() => { axios.get("/api/health").then(r => setData(r.data)); }, []);
-  return <div style={{padding:"2rem"}}><h1>app2oct24</h1><p>test</p><pre>{JSON.stringify(data,null,2)}</pre></div>;
+
+  useEffect(() => { 
+    api.get("/health").then(r => setData(r.data));
+  }, []);
+
+  return (
+    <div style={{padding:"2rem"}}>
+      <h1>app2oct24</h1>
+      <p>test</p>
+      <pre>{JSON.stringify(data,null,2)}</pre>
+    </div>
+  );
 }
